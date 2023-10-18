b38262fc4eca1e5b7ccde2dd4125dbf318fc80ee
Francis Ashley <francisashley.dev@gmail.com>
2023-10-18 21:36:30 +0100

    fix: highlight on custom cells doc

--- src/stories/custom-cells.story.vue
+++ src/stories/custom-cells.story.vue
@@ -14,7 +14,7 @@
           </template>
           <template #value-cell="props">
             <ValueCell v-bind="props" :style="{ background: 'blue' }">
-              <span v-html="props.cell.value" />
+              <span v-html="props.highlight(String(props.cell.value), props.highlightValue)" />
               [stuff]
             </ValueCell>
           </template>
