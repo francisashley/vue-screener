c74cc26fad9657c478a4c6d94b3122dcb252c73e
Francis Ashley <francisashley.dev@gmail.com>
2024-06-08 10:36:32 +0100

    add pick/omit stories

--- src/stories/2-configure-columns.story.vue
+++ src/stories/2-configure-columns.story.vue
@@ -1,6 +1,6 @@
 <template>
-  <Story title="2) Sticky Actions">
-    <Variant title="default" width="100">
+  <Story title="2) Configure columns">
+    <Variant title="Sticky Actions" width="100">
       <VueScreener :data="baseData" include-sticky-actions>
         <template #sticky-actions-head="props">
           <HeaderCell v-bind="props">Actions</HeaderCell>
@@ -14,6 +14,15 @@
           </ValueCell>
         </template>
       </VueScreener>
+    </Variant>
+    <Variant title="Pick fields" width="100">
+      <VueScreener :data="baseData" :pick="['id', 'first_name', 'last_name']" />
+    </Variant>
+    <Variant title="Omit fields" width="100">
+      <VueScreener :data="baseData" :omit="['id', 'first_name', 'last_name']" />
+    </Variant>
+    <Variant title="Pick + omit fields" width="100">
+      <VueScreener :data="baseData" :pick="['id', 'first_name', 'last_name']" :omit="['id']" />
     </Variant>
   </Story>
 </template>
