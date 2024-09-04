a6d6ba67218408b7e33884d2d0242733cad30229
Francis Ashley <francisashley.dev@gmail.com>
2024-09-05 00:27:24 +0300

    feat: export TableCell

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,8 @@ import Screener from './components/Screener.vue'
 import ScreenerPagination from './components/ScreenerPagination.vue'
 import ScreenerSearch from './components/ScreenerSearch.vue'
 import ScreenerSearchOptions from './components/ScreenerSearchOptions.vue'
+import TableCell from './components/ui/table/TableCell.vue'
 import { useScreener } from './hooks/use-screener'
 import './assets/tailwind.css'
 
-export { Screener, ScreenerSearch, ScreenerSearchOptions, ScreenerPagination, useScreener }
+export { Screener, ScreenerSearch, ScreenerSearchOptions, ScreenerPagination, TableCell, useScreener }
