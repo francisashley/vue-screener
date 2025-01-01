7967e54ba666e6de76322b066aeb9fead03e9cf2
Francis Ashley <francisashley.dev@gmail.com>
2025-01-01 15:09:27 +0000

    feat: switch lucide icons with codicon

--- src/components/icons/SortIcon.vue
+++ src/components/icons/SortIcon.vue
@@ -1,5 +1,6 @@
 <template>
-  <ArrowDown
+  <Icon
+    icon="codicon:arrow-down"
     :class="[
       twMerge('vsc-inline-flex vsc-items-center vsc-cursor-pointer vsc-h-[14px] vsc-w-[14px] vsc-rotate-0 vsc-transition vsc-ease-out vsc-duration-100', props.class), // eslint-disable-line
       direction === 'asc' && twMerge('vsc-rotate-[-180deg]', props.ascClass),
@@ -10,7 +11,7 @@
 <script lang="ts" setup>
 import { defineProps, HTMLAttributes } from 'vue'
 import { twMerge } from '../../utils/tailwind-merge.utils'
-import { ArrowDown } from 'lucide-vue-next'
+import { Icon } from '@iconify/vue'
 
 const props = defineProps<{
   direction?: null | 'asc' | 'desc'
--- src/components/search/VueScreenerSearchOptions.vue
+++ src/components/search/VueScreenerSearchOptions.vue
@@ -7,7 +7,7 @@
       :active-class="toggleButtonActiveClass"
       @click="toggleOption('case-sensitive')"
     >
-      <CaseSensitiveIcon :class="twMerge('vsc-w-4 vsc-h-4', props.iconClass)" />
+      <Icon icon="codicon:case-sensitive" :class="twMerge('vsc-w-4 vsc-h-4', props.iconClass)" />
     </ToggleButton>
     <ToggleButton
       title="Whole word"
@@ -16,7 +16,7 @@
       :active-class="toggleButtonActiveClass"
       @click="toggleOption('whole-word')"
     >
-      <WholeWordIcon :class="twMerge('vsc-w-4 vsc-h-4', props.iconClass)" />
+      <Icon icon="codicon:whole-word" :class="twMerge('vsc-w-4 vsc-h-4', props.iconClass)" />
     </ToggleButton>
     <ToggleButton
       title="Regular expression"
@@ -25,17 +25,17 @@
       :active-class="toggleButtonActiveClass"
       @click="toggleOption('regex')"
     >
-      <RegexIcon :class="twMerge('vsc-w-4 vsc-h-4', props.iconClass)" />
+      <Icon icon="codicon:regex" :class="twMerge('vsc-w-4 vsc-h-4', props.iconClass)" />
     </ToggleButton>
   </ToggleButtonGroup>
 </template>
 
 <script lang="ts" setup>
 import { VueScreener } from '@/interfaces/vue-screener'
-import { CaseSensitiveIcon, RegexIcon, WholeWordIcon } from 'lucide-vue-next'
 import ToggleButtonGroup from '../ui/toggle-button/ToggleButtonGroup.vue'
 import ToggleButton from '../ui/toggle-button/ToggleButton.vue'
 import { twMerge } from '../../utils/tailwind-merge.utils'
+import { Icon } from '@iconify/vue'
 
 const props = defineProps<{
   screener: VueScreener
